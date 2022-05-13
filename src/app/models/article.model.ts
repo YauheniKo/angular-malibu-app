d8dcc21650b9aa4306f23f192212bd190a151dea
eugenekontrovsky <riemi@bk.ru>
2022-05-13 13:27:06 +0300

    add getFiles with Article

--- src/app/models/article.model.ts
+++ src/app/models/article.model.ts
@@ -2,13 +2,15 @@ import {Tag} from "./tag.model";
 
 export class Article {
   id?: any;
-  userId?:any;
-  username?:string;
+  userId?: any;
+  username?: string;
   title?: string;
   description?: string;
   text?: string;
   tag?: Tag[];
-  likes?:any;
-  meLiked?:boolean;
+  likes?: any;
+  meLiked?: boolean;
   published?: boolean;
+  filesUrl?: Array<string> = []
+
 }
